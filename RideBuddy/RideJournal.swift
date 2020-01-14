baa416c4c5c0d09e173e1d00e81b0f94a425749f
markd <markd@borkware.com>
2020-01-14 14:48:03 -0500

    Sketches in decoding pipeline.

--- RideBuddy/RideJournal.swift
+++ RideBuddy/RideJournal.swift
@@ -3,25 +3,39 @@ import Combine
 
 struct RideJournalPayload {
     let flurbage = "Grongweezle"
+
+    init(webPayload payload: WebRideJournalPayload) {
+    }
+}
+
+struct WebRideJournalPayload: Codable {
 }
 
 struct RideJournal {
     private let networkQueue = DispatchQueue(label: "RideJournal",
         qos: .default, attributes: .concurrent)
 
+    var session: URLSession {
+        return URLSession.shared
+    }
+
+    private let decoder = JSONDecoder()
+
     func login(username: String,
-//        password: String) -> AnyPublisher<RideJournalPayload, Error> {
-        password: String) -> AnyPublisher<Data, URLError> {
+        password: String) -> AnyPublisher<RideJournalPayload, Error> {
 
         let url = URL(string: "https://www.cyclingfusiontrainingcenter.com/RideJournal/RJPlanner.aspx")!
 
-        let pipeline = URLSession.shared
+        let pipeline = session
         .dataTaskPublisher(for: url)
         .receive(on: networkQueue)
         .print("snorgle")
         .map(\.data)
+        .decode(type: WebRideJournalPayload.self, decoder: decoder)
+        .map { webPayload in
+            RideJournalPayload(webPayload: webPayload)
+        }
         .eraseToAnyPublisher()
-
 
         return pipeline
     }
