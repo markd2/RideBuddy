d9bd580109346a6fa81fc93789fa5a40132773a8
markd <markd@borkware.com>
2020-01-14 14:08:18 -0500

    Adds nascent login view

--- RideBuddy/ContentView.swift
+++ RideBuddy/ContentView.swift
@@ -1,16 +1,8 @@
-//
-//  ContentView.swift
-//  RideBuddy
-//
-//  Created by markd on 1/14/20.
-//  Copyright © 2020 markd. All rights reserved.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Flongwaffle")
+        LoginView()
     }
 }
 
