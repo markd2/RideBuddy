834dad4b48f4142fc3653edc2861933de9ec58e1
markd <markd@borkware.com>
2020-01-14 15:22:47 -0500

    Successfully sends login payload

--- RideBuddy/RideJournal.swift
+++ RideBuddy/RideJournal.swift
@@ -21,16 +21,50 @@ struct RideJournal {
 
     private let decoder = JSONDecoder()
 
+    func loginPayload() -> Data? {
+        let loginJSON =
+        """
+        {
+            "user": {
+                "password": "iwuvumikey",
+                "userName": "markd"
+            }
+        }
+        """
+
+        return loginJSON.data(using: .utf8)
+    }
+
     func login(username: String,
         password: String) -> AnyPublisher<RideJournalPayload, Error> {
 
-        let url = URL(string: "https://www.cyclingfusiontrainingcenter.com/RideJournal/RJPlanner.aspx")!
+        guard let payload = loginPayload() else {
+            fatalError("do something reasonable here")
+        }
+
+        let url = URL(string: "https://www.cyclingfusiontrainingcenter.com/RideJournal/RideBuddyServer.svc/Ajax/Download")!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.allHTTPHeaderFields = [
+            "Content-Type": "application/json",
+            "User-Agent" : "RideBuddy2"
+        ]
+
+        request.httpBody = payload
+        
 
         let pipeline = session
-        .dataTaskPublisher(for: url)
+        .dataTaskPublisher(for: request)
         .receive(on: networkQueue)
-        .print("snorgle")
+        .print()
         .map(\.data)
+        .print("snorgle")
+        .map { data in
+            let string = String(data: data, encoding: .utf8)
+            print("SNORGLE ", string)
+            return data
+        }
         .decode(type: WebRideJournalPayload.self, decoder: decoder)
         .map { webPayload in
             RideJournalPayload(webPayload: webPayload)
