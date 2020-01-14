bb45cd4fcb74393255e05ff7666246198e169340
markd <markd@borkware.com>
2020-01-14 14:17:44 -0500

    Configures login view

--- RideBuddy/LoginView.swift
+++ RideBuddy/LoginView.swift
@@ -1,7 +1,25 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var username: String = ""
+    @State var password: String = ""
+
     var body: some View {
-        return Text("SNORGLE")
+        VStack {
+            TextField("Username", text: $username)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .autocapitalization(.none)
+            .padding(.horizontal)
+
+            SecureField("Username", text: $password)
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .padding()
+
+            Button("Login", action: login)
+        }
+    }
+
+    private func login() {
+        print("Snorgle")
     }
 }
