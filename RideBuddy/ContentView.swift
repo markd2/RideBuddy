e0ab2a8352b5524bc375eec503e14ae4af6d893a
markd <markd@borkware.com>
2020-01-15 16:53:16 -0500

    Removes combine prototyping.

--- RideBuddy/ContentView.swift
+++ RideBuddy/ContentView.swift
@@ -1,52 +1,7 @@
 import SwiftUI
-import Combine
-
-
-class HeartRateThunk: ObservableObject {
-    private var subscriptions = Set<AnyCancellable>()
-
-    @Published var heartRate: String = "---"
-    @Published var batteryLevel: String = "---"
-
-    init(bluetoothAccess: BlueToothAccess? = nil) {
-        guard let bluetoothAccess = bluetoothAccess else {
-            return
-        }
-
-        bluetoothAccess.heartRatePublisher
-        .receive(on: RunLoop.main)
-        .map {
-            return String($0)
-        }
-        .assign(to: \.heartRate, on: self)
-        .store(in: &subscriptions)
-
-        bluetoothAccess.batteryLevelPublisher
-        .receive(on: RunLoop.main)
-        .map {
-            return String(Int($0 * 100))
-        }
-        .assign(to: \.batteryLevel, on: self)
-        .store(in: &subscriptions)
-    }
-}
 
 struct ContentView: View {
-    private var subscriptions = Set<AnyCancellable>()
-
-    @ObservedObject var thunk: HeartRateThunk = HeartRateThunk()
-
-    @Environment(\.bluetoothAccess) var bluetoothAccess: BlueToothAccess
     @Environment(\.defaultMeters) var defaultMeters: DefaultMeters
-
-    var heartRateMeterSource : MeterSource?
-    var heartRateMeterSource2x : MeterSource?
-    var batteryLevelMeterSource: MeterSource?
-
-    init() {
-//        thunk = HeartRateThunk(bluetoothAccess: bluetoothAccess)
-
-    }
 
     var body: some View {
         VStack {
