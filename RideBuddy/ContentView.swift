f433eb7f5bf1a093f60c832cbfc7cc4536e75efb
markd <markd@borkware.com>
2020-01-15 11:15:39 -0500

    Adds tabs, and placeholder tab for heart rate

--- RideBuddy/ContentView.swift
+++ RideBuddy/ContentView.swift
@@ -2,7 +2,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        LoginView()
+        TabView {
+            LoginView().tabItem {
+                VStack {
+                    Text("Login")
+                    Image(systemName: "desktopcomputer")
+                }
+            }
+            
+            Text("Snarnge").tabItem {
+                VStack {
+                    Text("Lub Dub")
+                    Image(systemName: "heart.circle.fill")
+                }
+            }
+            
+        }
     }
 }
 
