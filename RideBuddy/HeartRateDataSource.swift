8155c629ce2251268f70e980b74a3f1366b6b7ff
markd <markd@borkware.com>
2020-01-16 13:14:39 -0500

    Adds heart rate data source to hide some of the pipeline construction.

--- RideBuddy/HeartRateDataSource.swift
+++ RideBuddy/HeartRateDataSource.swift
@@ -1,18 +1,44 @@
 import Foundation
 import Combine
 
-protocol HeartRateDataSource {
-    init(withResolver: Resolver)
+
+class HeartRateDataSource: ServiceTypeResolvable {
+    var dataSource: DataSource
+
+    required init(resolver: Resolver) {
+        if let toof = resolver.maybeResolve(BlueToothAccess.self) {
+            dataSource = toof.heartRatePublisher
+            .receive(on: RunLoop.main)
+            .map {
+                return String($0)
+            }.eraseToAnyPublisher()
+        
+        } else {
+            dataSource = [1, 2, 3, 4].publisher
+            .map {
+                return String($0)
+            }.eraseToAnyPublisher()
+        }
+    }
 }
 
-class HeartRateDataSourceImpl: HeartRateDataSource {
-    required init(withResolver resolver: Resolver) {
-        if let toof = resolver.maybeResolve(BlueToothAccess.self) {
-            print("oop")
-        } else {
-            print("ack")
-        }
+class HeartRateDataSource2X: ServiceTypeResolvable {
+    var dataSource: DataSource
 
+    required init(resolver: Resolver) {
+        if let toof = resolver.maybeResolve(BlueToothAccess.self) {
+            dataSource = toof.heartRatePublisher
+            .receive(on: RunLoop.main)
+            .map {
+                return String($0 * 2)
+            }.eraseToAnyPublisher()
+        
+        } else {
+            dataSource = [1, 2, 3, 4].publisher
+            .map {
+                return String($0 * 2)
+            }.eraseToAnyPublisher()
+        }
     }
 }
 
