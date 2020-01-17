3b691d84efbfd9c395ac203f9f6eca6bf889f5c9
markd <markd@borkware.com>
2020-01-17 11:26:14 -0500

    my god it's full of rainbows

--- RideBuddy/StripChartView.swift
+++ RideBuddy/StripChartView.swift
@@ -2,6 +2,12 @@ import SwiftUI
 
 struct StripChartView: View {
     var body: some View {
-        Text("EVERYTHING IS GODDAMN BEAUTIFUL OH PLEASE MAKE IT STOP")
+        VStack(spacing: 0) {
+            Rectangle().fill(Color.red)
+            Rectangle().fill(Color.orange)
+            Rectangle().fill(Color.yellow)
+            Rectangle().fill(Color.green)
+            Rectangle().fill(Color.blue)
+        }
     }
 }
