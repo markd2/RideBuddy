8de1942b5cc6633c64ab27705d194c9d0600128d
markd <markd@borkware.com>
2020-01-17 11:39:52 -0500

    Hardcodes a random path.

--- RideBuddy/StripChartView.swift
+++ RideBuddy/StripChartView.swift
@@ -2,12 +2,29 @@ import SwiftUI
 
 struct StripChartView: View {
     var body: some View {
-        VStack(spacing: 0) {
-            Rectangle().fill(Color.red)
-            Rectangle().fill(Color.orange)
-            Rectangle().fill(Color.yellow)
-            Rectangle().fill(Color.green)
-            Rectangle().fill(Color.blue)
+        ZStack {
+            VStack(spacing: 0) {
+                Rectangle().fill(Color.red)
+                Rectangle().fill(Color.orange)
+                Rectangle().fill(Color.yellow)
+                Rectangle().fill(Color.green)
+                Rectangle().fill(Color.blue)
+            }
+            LineChart()
         }
+    }
+}
+
+struct LineChart: View {
+    var body: some View {
+        Path { path in
+            var y: CGFloat = 100
+            path.move(to: CGPoint(x: CGFloat(0), y: y))
+            for i in 0...250 {
+                y += CGFloat(Int.random(in: -3...3))
+                path.addLine(to: CGPoint(x: CGFloat(2 + i * 3), y: y))
+            }
+        }
+        .stroke(Color.black, lineWidth: 3)
     }
 }
